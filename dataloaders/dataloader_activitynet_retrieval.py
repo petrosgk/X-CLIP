8033b5c339a65e5b9f17a5661ecc66e9c276c9f8
petros <petros@tmc2.ai>
2023-09-09 13:03:47 +0300

    replace np.long with np.longlong

--- dataloaders/dataloader_activitynet_retrieval.py
+++ dataloaders/dataloader_activitynet_retrieval.py
@@ -127,11 +127,11 @@ class ActivityNet_DataLoader(Dataset):
         k = 1
         r_ind = [sub_id]
 
-        starts = np.zeros(k, dtype=np.long)
-        ends = np.zeros(k, dtype=np.long)
-        pairs_text = np.zeros((k, self.max_words), dtype=np.long)
-        pairs_mask = np.zeros((k, self.max_words), dtype=np.long)
-        pairs_segment = np.zeros((k, self.max_words), dtype=np.long)
+        starts = np.zeros(k, dtype=np.longlong)
+        ends = np.zeros(k, dtype=np.longlong)
+        pairs_text = np.zeros((k, self.max_words), dtype=np.longlong)
+        pairs_mask = np.zeros((k, self.max_words), dtype=np.longlong)
+        pairs_segment = np.zeros((k, self.max_words), dtype=np.longlong)
 
         for i in range(k):
             ind = r_ind[i]
@@ -163,7 +163,7 @@ class ActivityNet_DataLoader(Dataset):
         return pairs_text, pairs_mask, pairs_segment, starts, ends
 
     def _get_rawvideo(self, idx, s, e):
-        video_mask = np.zeros((len(s), self.max_frames), dtype=np.long)
+        video_mask = np.zeros((len(s), self.max_frames), dtype=np.longlong)
         max_video_length = [0] * len(s)
 
         # Pair x L x T x 3 x H x W
